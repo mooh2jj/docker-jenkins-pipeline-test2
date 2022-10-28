66faeb3d3c36de5e579a6a416d9eb5e8f84e5016
seonggon do <62453668+mooh2jj@users.noreply.github.com>
2022-10-28 13:37:12 +0900

    first commit

---
+++ src/main/java/com/example/dockerjenkinspipelinetest2/DockerJenkinsPipelineTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.dockerjenkinspipelinetest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsPipelineTest2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerJenkinsPipelineTest2Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/dockerjenkinspipelinetest2/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.dockerjenkinspipelinetest2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test() {
+        return "jenkins_test_작업을 시작!";
+    }
+
+}
